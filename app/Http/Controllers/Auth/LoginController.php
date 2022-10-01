7cab1835b59b120f1656c122e76938d761906ef8
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-10-02 01:11:27 +0800

    Handle logout tokens revoke

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Passport\TokenRepository;
+use Laravel\Passport\RefreshTokenRepository;
+use Laravel\Passport\Bridge\AuthCodeRepository;
+use Laravel\Passport\Passport;
 
 class LoginController extends Controller
 {
@@ -36,5 +42,43 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request)
+    {
+        $tokenRepository = app(TokenRepository::class);
+        $refreshTokenRepository = app(RefreshTokenRepository::class);
+        $authCodeRepository = app(AuthCodeRepository::class);
+
+        $accessTokens = Passport::token()->where('user_id', auth()->user()->id)->where('revoked', false)->get();
+
+        if($accessTokens->count() > 0) {
+            foreach($accessTokens as $accessToken) {
+                $tokenRepository->revokeAccessToken($accessToken->id);
+                $refreshTokenRepository->revokeRefreshTokensByAccessTokenId($accessToken->id);
+            }
+        }
+
+        $authCodes = Passport::authCode()->where('user_id', auth()->user()->id)->where('revoked', false)->get();
+
+        if($authCodes->count() > 0) {
+            foreach($authCodes as $authCode) {
+                $authCodeRepository->revokeAuthCode($authCode->id);
+            }
+        }
+
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
     }
 }
